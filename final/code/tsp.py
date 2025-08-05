94e25944b99c16fe271536e09749b6ea423654f7
Jack Duggan <jacksdggn@gmail.com>
2025-08-05 11:37:30 +0930

    docs: update constructor doc string

--- final/code/tsp.py
+++ final/code/tsp.py
@@ -5,6 +5,7 @@ class TSP:
 
     takes path to test case as input
     edits private variable 'graph'
+    graph is 2d array, where graph[i][j] = distance between i and j
     """
     self.graph = [[]]
     return
