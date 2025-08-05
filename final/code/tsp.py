42a8c078756d7017333ac766e80a809eda1cff85
Jack Duggan <jacksdggn@gmail.com>
2025-08-05 11:29:49 +0930

    feat: class def

--- final/code/tsp.py
+++ final/code/tsp.py
@@ -1,4 +1,10 @@
 class TSP:
-  #constructor
-  def __init__(self):
+  
+  def __init__(self, path):
+    """Init class
+
+    takes path to test case as input
+    edits private variable 'graph'
+    """
+    self.graph = [[]]
     return
