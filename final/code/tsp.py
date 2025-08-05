dba1702762708638bfe166350b9550cd45f48176
Jack Duggan <jacksdggn@gmail.com>
2025-08-05 11:54:14 +0930

    feat: add cost helper function

--- final/code/tsp.py
+++ final/code/tsp.py
@@ -1,5 +1,5 @@
 class TSP:
-  
+  graph = [[]]
   def __init__(self, path):
     """Init class
 
@@ -9,3 +9,26 @@ class TSP:
     """
     self.graph = [[]]
     return
+  
+  def cost(self, perm):
+    """Cost
+    
+    given a permutation, uses graph to calculate total cost
+    """
+    cost = 0
+    for i in range(len(perm) - 1):
+      cost += self.graph[perm[i]][perm[i + 1]]
+    cost += self.graph[perm[-1]][perm[0]]
+      
+
+
+
+
+  def swap(self, perm, cost):
+    """Swap
+    
+    uses class variable graph, along with an initial permutation/cost to 
+    test for a better cost using the swap neighbourhood search.
+    """
+
+    return solution
