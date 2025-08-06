ed49d0fd62aa8ee617b497681ada7dec307206af
Jayom Raval <a1852664@adelaide.edu.au>
2025-08-06 20:36:01 +0930

    merge with main

--- final/code/tsp.py
+++ final/code/tsp.py
@@ -1,4 +1,205 @@
+import random
+import os
+import csv
+import typing
+
 class TSP:
-  #constructor
-  def __init__(self):
-    return
+    graph = [[]]
+    saveFile: typing.TextIO
+    csvWriter: csv.writer
+
+    def __init__(self, testPath, savePath):
+        """Init class
+
+        takes path to test case as input
+        edits private variable 'graph'
+        graph is 2d array, where graph[i][j] = distance between i and j
+        """
+        self.graph = [[]]
+        self.loadSaveFile(savePath)
+        return
+
+    def random_pairs(self, n):
+         # Generate all unique index pairs (i, j) where i < j
+        pairs = [(i, j) for i in range(n - 1) for j in range(i + 1, n)]
+        random.shuffle(pairs)  # Randomize the order of swaps
+        return pairs
+
+    def delta_swap_cost(self, perm, cost, i, j):
+        """
+        Calculate the new tour cost if cities at positions i and j are swapped,
+        using delta evaluation instead of recalculating the whole cost.
+
+        Args:
+            perm (list[int]): Current tour
+            cost (float): Cost of the tour
+            i (int): First swap position
+            j (int): Second swap position
+
+        Returns:
+            float: New tour cost after swapping i and j"""
+        n = len(perm)
+
+        # Wrap-around for circular tour
+        a, b = perm[(i - 1) % n], perm[i]
+        c = perm[(i + 1) % n]
+        d, e = perm[(j - 1) % n], perm[j]
+        f = perm[(j + 1) % n]
+
+        # Adjacent swap case
+        if j == i + 1 or (i == 0 and j == n - 1):
+            old_cost = self.graph[a][b] + self.graph[e][f]
+            new_cost = self.graph[a][e] + self.graph[b][f]
+        else:  # Non-adjacent case
+            old_cost = (
+                self.graph[a][b] + self.graph[b][c] +
+                self.graph[d][e] + self.graph[e][f]
+            )
+            new_cost = (
+                self.graph[a][e] + self.graph[e][c] +
+                self.graph[d][b] + self.graph[b][f]
+            )
+
+        return cost + (new_cost - old_cost)
+
+    def exchange(self, perm, cost):
+        """
+        Perform the exchange neighbourhood search until a better solution is found
+        if no better solution is found, returns input permutation and cost.
+        Algorithm is greedy, so swap paris are done randomly
+        Args:
+            perm (list[int]): Current tour
+            cost (float): Cost of the tour
+        Returns:
+            (list[int], float): The improved permutation and its cost.
+        """
+        # create a copy of perm, not a reference
+        sol = perm.copy()
+        n = len(sol)
+
+        pairs = self.random_pairs(n)
+
+        for i, j in pairs:
+            n_cost = self.delta_swap_cost(sol, cost, i, j)
+            if n_cost < cost:
+                return sol, cost
+        return sol, cost
+    
+    def inversion(self, perm, cost):
+        perm_ = perm.copy()
+        
+        pairs = self.random_pairs(len(perm_))
+        
+        for i, j in pairs:
+            old_cost = 0
+            for i in range(i, j - 1):
+                old_cost += self.graph[perm_[i]][perm_[i + 1]]
+            
+            # Perform inversion (inclusive of j as random pairs has j < n)
+            for k, l in zip(range(i, j), range(j, i, -1)):
+                temp = perm_[k]
+                perm_[k] = perm_[l]
+                perm_[l] = perm_[k]
+            
+            cost = 0
+            for i in range(i, j - 1):
+                cost += self.graph[perm_[i]][perm_[i + 1]]
+                
+            if cost < old_cost:
+                return perm_, cost
+        
+        return perm_, cost
+    
+    def localSearch(self, nIterations):
+        """
+        Performs localSearch to determine an optimised route to the Traveling Salesman Problem 
+        Results are saved to a csv file for processing later
+
+        Args:
+            nIterations (striintng): The number of attempts the algorithm will have to produce an optimised value 
+
+        Returns:
+            """
+        
+        for i in range(nIterations):
+            basePerm = [1, 2, 3]  # ADD: Generate the permutation
+            baseCost = 10 # ADD: Calculate the overall cost
+
+            # Calculate results for the jump
+            jumpCost = baseCost
+            jumpPerm = basePerm
+            '''while True:
+                jumpPerm, tempCost = self.jump(jumpPerm, jumpCost)
+                if (tempCost < jumpCost):
+                    jumpCost = tempCost
+                else:
+                    break'''
+
+            # Calculate results for the exchange
+            exchCost = baseCost
+            exchPerm = basePerm
+            while True:
+                exchPerm, tempCost = self.exchange(exchPerm, exchCost)
+                if (tempCost < exchCost):
+                    exchCost = tempCost
+                else:
+                    break
+
+            # Calculate results for the inversion
+            invsCost = baseCost
+            invsPerm = basePerm
+            while True:
+                invsPerm, tempCost = self.inversion(invsPerm, invsCost)
+                if (tempCost < invsCost):
+                    invsCost = tempCost
+                else:
+                    break
+
+
+            self.saveData(jumpPerm, jumpCost, exchPerm, exchCost, invsPerm, invsCost)
+
+
+        
+    def loadSaveFile(self, filePath):
+        """
+        Prepares the save file for the algorithm. It creates a new file if required, and loads it into memory. 
+        If the file already exists, then a new file will be created with a slightly modified name (#)
+
+        Args:
+            filePath (string): file path to the csv file where the data will be saved. 
+
+        Returns:
+            """
+        
+        # if the file exists, give it a unique name so that it does not get overriden
+        tempFileName = filePath
+        pos = len(filePath) - 4 # ignore the .csv at the end
+        n = 1
+        while os.path.exists(tempFileName):
+            tempFileName = filePath[:pos] + '(' + n.__str__() + ')' + filePath[pos:]
+            n += 1
+        filePath = tempFileName
+
+        # Generate the file and the csv writer for use
+        self.saveFile = open(filePath, 'w', newline='')
+        self.csvWriter = csv.writer(self.saveFile)
+        self.csvWriter.writerow(['Jump - tour', 'Jump - cost', 'Exchange - tour', 'Exchange - cost', 'Inverse - tour', 'Inverse - cost'])
+        self.saveFile.flush()
+
+    def saveData(self, jumpTour, jumpCost, exchangeTour, exchangeCost, inverseTour, inverseCost):
+        """
+        Saves an entry of data into the csv file specified in the constructor. 
+        This entry has the result for the three different switching types. 
+
+        Args:
+            jumpTour (list[int]): the calculated best tour when using jump
+            jumpCost (int): the calculated cos for the tour found using jump 
+            exchangeTour (list[int]): the calculated best tour when using exchange
+            exchangeCost (int): the calculated cos for the tour found using exchange 
+            inverseTour (list[int]): the calculated best tour when using inverse
+            inverseCost (int): the calculated cos for the tour found using inverse 
+
+        Returns:
+            """
+        self.csvWriter.writerow([jumpTour, jumpCost, exchangeTour, exchangeCost, inverseTour, inverseCost])
+        self.saveFile.flush()
