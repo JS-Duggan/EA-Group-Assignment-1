1ff9e8be0c36f5d2fb84a217a14e118e6106d0d6
Cooper Whitbread <cooper.whitbread@adam.com.au>
2025-08-06 15:48:26 +0930

    Added general local search approach, not yet tested

---
+++ final/code/runLocalSearch.py
@@ -0,0 +1,9 @@
+from tsp import TSP
+
+# number of tests for each example
+n = 30
+
+EIL51 = TSP("final/code/test_cases/eli51", "final/code/temp_results/eil51.csv")
+#EIL51.localSearch(n)
+EIL51.saveData(1, 2, 3, 4, 5, 6)
+
