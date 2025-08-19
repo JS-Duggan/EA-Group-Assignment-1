7fc1f9a4b8157249a2704288f974092eef75c80b
Jack Duggan <jacksdggn@gmail.com>
2025-08-19 11:09:45 +0930

    feat: implement cycle crossover

--- final/code/crossover.py
+++ final/code/crossover.py
@@ -81,7 +81,6 @@ class Crossover:
                 child_1[index] = parent_2[index]
             index += 1
 
-
         # Perform for parent 2
         #   copy segment to child
         child_2 = [0] * size
@@ -107,5 +106,74 @@ class Crossover:
             if child_2[index] == 0:
                 child_2[index] = parent_1[index]
             index += 1
+
+        return child_1, child_2
+    
+    def cycle_crossover(self, parent_1, parent_2):
+
+        # helper function to find start index of next cycle
+        def get_index(lst):
+            for x in lst:
+                if x != -1:
+                    return x
+            return -1
+        
+        size = len(parent_1)
+
+        # get cycles for p1
+        p1_cycles = []
+        indices = list(range(0, size))
+
+        while True:
+            cycle = [0] * size
+            index = get_index(indices)
+            if index == -1:
+                break
+            while True:
+                if cycle[index] != 0:
+                    break
+                val = parent_1[index]
+                cycle[index] = val
+                indices[index] = -1
+                index = parent_2.index(val)
+            p1_cycles.append(cycle)
+
+        # get cycles for p2
+        p2_cycles = []
+        print(p1_cycles)     
+        indices = list(range(0, size))
+
+        while True:
+            cycle = [0] * size
+            index = get_index(indices)
+            if index == -1:
+                break
+            while True:
+                if cycle[index] != 0:
+                    break
+                val = parent_2[index]
+                cycle[index] = val
+                indices[index] = -1
+                index = parent_1.index(val)
+            p2_cycles.append(cycle)
+
+        print(p2_cycles)
+
+        child_1 = [0] * size
+        child_2 = [0] * size
+        for i in range(len(p1_cycles)):
+            for j in range(size):
+                if i % 2 == 0:
+                    for k in range(size):
+                        if p1_cycles[i][k] != 0:
+                            child_1[k] = p1_cycles[i][k]
+                        if p2_cycles[i][k] != 0:
+                            child_2[k] = p2_cycles[i][k]
+                else:
+                    for k in range(size):
+                        if p1_cycles[i][k] != 0:
+                            child_2[k] = p1_cycles[i][k]
+                        if p2_cycles[i][k] != 0:
+                            child_1[k] = p2_cycles[i][k]
 
         return child_1, child_2
