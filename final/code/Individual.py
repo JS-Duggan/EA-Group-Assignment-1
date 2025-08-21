593b80738c0171ec123f4a1ae81b4342723d5e1c
Cooper Whitbread <cooper.whitbread@adam.com.au>
2025-08-21 15:23:49 +0930

    Added individual class with inheritance to access mutation algorithms

--- final/code/Individual.py
+++ final/code/Individual.py
@@ -1,3 +1,11 @@
 import random
+from permutation import Permutation
+from crossover import Crossover
 
+class Individual(Permutation):
+    crossover: Crossover
 
+    def __init__(self):
+        return
+    
+    
