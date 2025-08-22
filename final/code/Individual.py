8b8b6989abc9d5719f84cca4ec69874c0210946a
Jayom Raval <a1852664@adelaide.edu.au>
2025-08-22 18:00:56 +0930

    Individual class unfinished

--- final/code/Individual.py
+++ final/code/Individual.py
@@ -1,3 +1,9 @@
-import random
+from tsp import TSP
 
+class Individual:
+    def generate_single_permutation(self, num_nodes):
+        individual_perm = TSP()
+        individual_perm.generate_random_path(self, num_nodes)
+        return individual_perm
+    
 
