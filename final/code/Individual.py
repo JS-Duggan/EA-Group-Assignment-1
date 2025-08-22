c995ca762857b98ed70847c8bbd413a235a6960f
Jayom Raval <a1852664@adelaide.edu.au>
2025-08-22 18:12:11 +0930

    class Individual - added generate_single_permutation method

--- final/code/Individual.py
+++ final/code/Individual.py
@@ -1,22 +1,16 @@
-<<<<<<< HEAD
+import random
+from permutation import Permutation
+from crossover import Crossover
 from tsp import TSP
 
-class Individual:
+class Individual(Permutation):
+    crossover: Crossover
     def generate_single_permutation(self, num_nodes):
         individual_perm = TSP()
         individual_perm.generate_random_path(self, num_nodes)
         return individual_perm
-    
-=======
-import random
-from permutation import Permutation
-from crossover import Crossover
-
-class Individual(Permutation):
-    crossover: Crossover
->>>>>>> origin/main
-
     def __init__(self):
         return
     
     
+
